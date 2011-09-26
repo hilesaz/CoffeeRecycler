a689ab58f20385feeb07a7cb1d8b0a63eaf0aa24
Bovine <hilesaz@gmail.com>
2011-09-26 01:53:52 -0700

    Test again and again

---
+++ src/Recycler.java
@@ -0,0 +1,13 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Hilesaz
+ * Date: 9/26/11
+ * Time: 1:52 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Recycler {
+    public static int main(String[] args)
+    {
+        return 0;
+    }
+}
